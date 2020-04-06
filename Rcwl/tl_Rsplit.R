497afa01406266bc168f56bd4b43b9e0f1368862
Qiang Hu <qiang.hu@roswellpark.org>
2020-04-06 18:25:36 -0400

    Add fix rownames

--- Rcwl/tl_Rsplit.R
+++ Rcwl/tl_Rsplit.R
@@ -1,10 +1,14 @@
 #' split and combine multiple files by column names
-rsplit <- function(files, columns, cnames = NULL, outfile, sep = "\t", isep = ","){
+rsplit <- function(files, columns, cnames = NULL, outfile, sep = "\t", isep = ",",
+                   fixName = TRUE){
     files <- strsplit(files, split = isep)[[1]]
     columns <- strsplit(columns, split = isep)[[1]]
     expL <- lapply(files, function(x){
         exp1 <- read.table(x, sep = sep, header = TRUE, row.names = 1,
                            check.names = FALSE, colClasses = "character")
+        if(fixName){
+            rownames(exp1) <- sub("\\|.*", "", rownames(exp1))
+        }
         exp1[, columns, drop = F]
     })
     Exp <- do.call(cbind, expL)
@@ -15,7 +19,8 @@ rsplit <- function(files, columns, cnames = NULL, outfile, sep = "\t", isep = ",
     write.table(Exp, file = outfile, quote = FALSE, sep = sep)
 }
 
-p1 <- InputParam(id = "files", type = "File[]", prefix = "files=", separate = F, itemSeparator = ",")
+p1 <- InputParam(id = "files", type = "File[]",
+                 prefix = "files=", separate = F, itemSeparator = ",")
 p2 <- InputParam(id = "columns", type = "string", prefix = "columns=", separate = F)
 p3 <- InputParam(id = "cnames", type = "string?", prefix = "cnames=", separate = F)
 p4 <- InputParam(id = "outfile", type = "string", prefix = "outfile=", separate = F)
