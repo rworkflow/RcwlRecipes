e460876a1f725df33aa1c1aeeb81320e72f17242
Qiang Hu <qiang.hu@roswellpark.org>
2020-06-03 19:37:39 -0400

    cnv

--- Rcwl/tl_cnv_facets.R
+++ Rcwl/tl_cnv_facets.R
@@ -1,14 +1,15 @@
 ## cnv_facets
-p1 <- InputParam(id = "tbam", type = "File", prefix = "-t", secondaryFiles = ".bai")
-p2 <- InputParam(id = "nbam", type = "File", prefix = "-n", secondaryFiles = ".bai")
-p3 <- InputParam(id = "vcf", type = "File", prefix = "-vcf", secondaryFiles = ".tbi")
-p4 <- InputParam(id = "out", type = "string", prefix = "-o")
-p5 <- InputParam(id = "build", type = "string", prefix = "-g")
-p6 <- InputParam(id = "targets", type = "File", prefix = "-T")
+p1 <- InputParam(id = "tbam", type = "File?", prefix = "-t", secondaryFiles = ".bai")
+p2 <- InputParam(id = "nbam", type = "File?", prefix = "-n", secondaryFiles = ".bai")
+p3 <- InputParam(id = "vcf", type = "File?", prefix = "-vcf", secondaryFiles = ".tbi")
+p4 <- InputParam(id = "pileup", type = "File?", prefix = "-p")
+p5 <- InputParam(id = "out", type = "string", prefix = "-o")
+p6 <- InputParam(id = "build", type = "string", prefix = "-g")
+p7 <- InputParam(id = "targets", type = "File", prefix = "-T")
 o1 <- OutputParam(id = "Out", type = "File[]", glob = "$(inputs.out)*")
 req1 <- list(class = "DockerRequirement",
              dockerPull = "quay.io/biocontainers/cnv_facets:0.16.0--py38r36h4b26f60_1")
 cnv_facets <- cwlParam(baseCommand = "cnv_facets.R",
                        requirements = list(req1),
-                       inputs = InputParamList(p1, p2, p3, p4, p5, p6),
+                       inputs = InputParamList(p1, p2, p3, p4, p5, p6, p7),
                        outputs = OutputParamList(o1))
---
+++ Rcwl/tl_samtools_depth.R
@@ -0,0 +1,10 @@
+
+p1 <- InputParam(id = "bam", type = "File")
+o1 <- OutputParam(id = "pileup", type = "File", glob = "$(inputs.bam.nameroot).depth.txt")
+req1 <- list(class = "DockerRequirement",
+             dockerPull = "biocontainers/samtools:v1.7.0_cv3")
+samtools_depth <- cwlParam(baseCommand = c("samtools", "depth"),
+                             requirements = list(req1),
+                             inputs = InputParamList(p1),
+                             outputs = OutputParamList(o1),
+                             stdout = "$(inputs.bam.nameroot).depth.txt")
