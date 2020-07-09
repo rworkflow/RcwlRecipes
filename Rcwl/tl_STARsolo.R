48e1a85116c24089ad22902b454d2b6fecd9ed72
Qian Liu <qliu7@buffalo.edu>
2020-07-09 17:37:38 -0400

    Update STARsolo with multiple lane file input

--- Rcwl/tl_STARsolo.R
+++ Rcwl/tl_STARsolo.R
@@ -1,25 +1,28 @@
+## Ref: https://github.com/alexdobin/STAR/blob/master/docs/STARsolo.md
 
-p1 <- InputParam(id = "readFilesIn", type = "File[]", prefix = "--readFilesIn")
-p2 <- InputParam(id = "genomeDir", type = "Directory", prefix = "--genomeDir")
-p3 <- InputParam(id = "whiteList", type = "File", prefix = "--soloCBwhitelist")
-p4 <- InputParam(id = "soloType", type = "string", prefix = "--soloType",
+p1 <- InputParam(id = "readFilesIn_cdna", type = "File[]", prefix = "--readFilesIn",
+                 position = 1, itemSeparator = ",")
+p2 <- InputParam(id = "readFilesIn_cb", type = "File[]", position = 2, itemSeparator = ",")
+p3 <- InputParam(id = "genomeDir", type = "Directory", prefix = "--genomeDir")
+p4 <- InputParam(id = "whiteList", type = "File", prefix = "--soloCBwhitelist")
+p5 <- InputParam(id = "soloType", type = "string", prefix = "--soloType",
                  default = "CB_UMI_Simple")
-p5 <- InputParam(id = "soloUMIlen", type = "string", prefix = "--soloUMIlen",
+p6 <- InputParam(id = "soloUMIlen", type = "string", prefix = "--soloUMIlen",
                  default = "12")
-p6 <- InputParam(id = "runThreadN", type = "int", prefix = "--runThreadN", default = 1L)
-p7 <- InputParam(id = "sOut", type = "string", prefix = "--soloOutFileNames",
-                 default = "Solo_out")
-o1 <- OutputParam(id = "outBam", type = "File", glob = "*.bam")
-o2 <- OutputParam(id = "outLog", type = "File[]", glob = "*.out")
-o3 <- OutputParam(id = "SJ", type = "File", glob = "*out.tab")
-o4 <- OutputParam(id = "Solo", type = "Directory", glob = "$(inputs.sOut)Gene")
+p7 <- InputParam(id = "runThreadN", type = "int", prefix = "--runThreadN", default = 1L)
 
-req1 <- list(class = "DockerRequirement",
-             dockerPull = "quay.io/biocontainers/star:2.7.3a--0")
+o1 <- OutputParam(id = "outBam", type = "File", glob = "*.bam")
+o2 <- OutputParam(id = "outLog", type = "File[]", glob = "Log*")
+o3 <- OutputParam(id = "SJ", type = "File", glob = "SJ.out.tab")
+o4 <- OutputParam(id = "Solo", type = "Directory", glob = "Solo.out")
+
 STARsolo <- cwlParam(baseCommand = "STAR",
-                     requirements = list(req1),
+                     requirements = list(requireDocker("quay.io/biocontainers/star:2.7.5a--0")),
                      arguments = list("--outSAMunmapped", "Within",
                                       "--readFilesCommand", "zcat",
-                                      "--outSAMtype", "BAM", "SortedByCoordinate"),
+                                      "--outSAMtype", "BAM", "SortedByCoordinate",
+                                      "--soloUMIfiltering", "MultiGeneUMI",
+                                      "--soloCBmatchWLtype", "1MM_multi_pseudocounts"),
                      inputs = InputParamList(p1, p2, p3, p4, p5, p6, p7),
                      outputs = OutputParamList(o1, o2, o3, o4))
+
