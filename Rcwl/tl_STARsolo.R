7f6becb07147120a67167a56e9aba9045ea92e93
Qian Liu <qliu7@buffalo.edu>
2020-07-15 15:11:41 -0400

    Update the sam related params for docker

--- Rcwl/tl_STARsolo.R
+++ Rcwl/tl_STARsolo.R
@@ -11,16 +11,16 @@ p6 <- InputParam(id = "soloUMIlen", type = "string", prefix = "--soloUMIlen",
                  default = "12")
 p7 <- InputParam(id = "runThreadN", type = "int", prefix = "--runThreadN", default = 1L)
 
-o1 <- OutputParam(id = "outBam", type = "File", glob = "*.bam")
+o1 <- OutputParam(id = "outAlign", type = "File", glob = "*.sam")
 o2 <- OutputParam(id = "outLog", type = "File[]", glob = "Log*")
 o3 <- OutputParam(id = "SJ", type = "File", glob = "SJ.out.tab")
 o4 <- OutputParam(id = "Solo", type = "Directory", glob = "Solo.out")
 
 STARsolo <- cwlParam(baseCommand = "STAR",
                      requirements = list(requireDocker("quay.io/biocontainers/star:2.7.5a--0")),
-                     arguments = list("--outSAMunmapped", "Within",
+                     arguments = list(## "--outSAMunmapped", "Within",
                                       "--readFilesCommand", "zcat",
-                                      "--outSAMtype", "BAM", "SortedByCoordinate",
+                                      ## "--outSAMtype", "BAM", "SortedByCoordinate",
                                       "--soloUMIfiltering", "MultiGeneUMI",
                                       "--soloCBmatchWLtype", "1MM_multi_pseudocounts"),
                      inputs = InputParamList(p1, p2, p3, p4, p5, p6, p7),
