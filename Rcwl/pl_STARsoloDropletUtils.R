765ff21e45365150ef679adeaa2d539b8d09fa9f
Qian Liu <qliu7@buffalo.edu>
2020-07-16 12:15:15 -0400

    merge STARsolo

---
+++ Rcwl/pl_STARsoloDropletUtils.R
@@ -0,0 +1,36 @@
+p1 <- InputParam(id = "fastq_cdna", type = "File[]")  ## only "id" names will be used for "Step".
+p2 <- InputParam(id = "fastq_cb", type = "File[]")
+p3 <- InputParam(id = "genomeDir", type = "Directory")
+p4 <- InputParam(id = "whiteList", type = "File")
+p5 <- InputParam(id = "runThreadN", type = "int")
+
+@include tl_STARsolo.R tl_DropletUtils.R 
+## STARsolo
+s1 <- Step(id = "STARsolo", run = STARsolo,
+           In = list(readFilesIn_cdna = "fastq_cdna", 
+                     readFilesIn_cb = "fastq_cb",
+                     genomeDir = "genomeDir",
+                     whiteList = "whiteList",
+                     runThreadN = "runThreadN"))
+
+## DropletUtils
+s2 <- Step(id = "DropletUtils", run = DropletUtils,
+           In = list(dirname = "STARsolo/Solo"))
+
+## outputs
+o1 <- OutputParam(id = "sam", type = "File", outputSource = "STARsolo/outAlign")
+o2 <- OutputParam(id = "Solo", type = "Directory", outputSource = "STARsolo/Solo")
+o3 <- OutputParam(id = "sce", type = "File", outputSource = "DropletUtils/outsce")
+o4 <- OutputParam(id = "plots", type = "File", outputSource = "DropletUtils/plots")
+
+## stepParam
+STARsoloDropletUtils <- cwlStepParam(inputs = InputParamList(p1, p2, p3, p4, p5),
+                                     outputs = OutputParamList(o1, o2, o3, o4))
+## pipeline
+STARsoloDropletUtils <- STARsoloDropletUtils + s1 + s2
+
+
+
+
+
+
---
+++ Rcwl/tl_DropletUtils.R
@@ -0,0 +1,33 @@
+DropletUtils <- function(dir.name, lower=100, df=20, ...) {    
+    library(DropletUtils)
+    dir.name <- file.path(dir.name, "Gene/raw")
+    sce <- read10xCounts(dir.name, ...)
+    br.out <- barcodeRanks(assay(sce), lower=lower, df=df)
+    o <- order(br.out$rank)
+    e.out <- emptyDrops(assay(sce))
+    is.cell <- e.out$FDR <= 0.01
+    pdf(file = "diagnostics.pdf")
+    plot(br.out$rank, br.out$total, log="xy", xlab="Rank", ylab="Total",
+         title = "Barcode Ranks")
+    lines(br.out$rank[o], br.out$fitted[o], col="red")
+    abline(h=metadata(br.out)$knee, col="dodgerblue", lty=2)
+    abline(h=metadata(br.out)$inflection, col="forestgreen", lty=2)
+    legend("bottomleft", lty=2, col=c("dodgerblue", "forestgreen"),
+           legend=c("knee", "inflection"))
+    plot(e.out$Total, -e.out$LogProb, col=ifelse(is.cell, "red", "black"),
+         xlab="Total UMI count", ylab="-Log Probability",
+         title = "Empty Droplets")
+    dev.off()
+    sce1 <- sce[, which(is.cell == "TRUE")]
+    saveRDS(sce1, file = "sce_filtered.rds")
+}
+
+## write out the R function as `cwlParam`. 
+p1 <- InputParam(id = "dirname", type = "Directory", prefix = "dir.name=", separate = FALSE)
+p2 <- InputParam(id = "lower", type = "int", prefix = "lower=", separate = FALSE, default = 100L)
+p3 <- InputParam(id = "df", type = "int", prefix = "df=", separate = FALSE, default = 20L)
+o1 <- OutputParam(id = "plots", type = "File", glob = "*.pdf")
+o2 <- OutputParam(id = "outsce", type = "File", glob = "*.rds")
+DropletUtils <- cwlParam(baseCommand = DropletUtils,
+                      inputs = InputParamList(p1, p2, p3),
+                      outputs = OutputParamList(o1, o2))
