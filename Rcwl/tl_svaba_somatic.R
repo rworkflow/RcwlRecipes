1b806ff32c9363e28f7f0ef6cec78228ebb8513b
qhu75 <qhu75@gatech.edu>
2021-02-19 17:20:39 -0500

    update svaba with capture region

--- Rcwl/tl_svaba_somatic.R
+++ Rcwl/tl_svaba_somatic.R
@@ -1,10 +1,11 @@
 ## https://github.com/walaj/svaba
 p1 <- InputParam(id = "tbam", type = "File", prefix = "-t", secondaryFiles = ".bai")
 p2 <- InputParam(id = "nbam", type = "File", prefix = "-n", secondaryFiles = ".bai")
-p3 <- InputParam(id = "dbsnp", type = "File", prefix = "-D")
-p4 <- InputParam(id = "ref", type = "File", prefix = "-G",
+p3 <- InputParam(id = "target", type = "File?", prefix = "-k")
+p4 <- InputParam(id = "dbsnp", type = "File", prefix = "-D")
+p5 <- InputParam(id = "ref", type = "File", prefix = "-G",
                  secondaryFiles = c(".amb", ".ann", ".bwt", ".pac", ".sa", ".fai"))
-p5 <- InputParam(id = "cores", type = "int", prefix = "-p", default = 4L)
+p6 <- InputParam(id = "cores", type = "int", prefix = "-p", default = 4L)
 o1 <- OutputParam(id = "raw", type = "File", glob = "*.bps.txt.gz")
 o2 <- OutputParam(id = "contig", type = "File", glob = "*.contigs.bam")
 o3 <- OutputParam(id = "discordants", type = "File", glob = "*.discordant.txt.gz")
@@ -16,5 +17,5 @@ req1 <- list(class = "DockerRequirement",
 svaba_somatic <- cwlProcess(baseCommand = c("svaba", "run"),
                           requirements = list(req1),
                           arguments = list("-a", "somatic_run"),
-                          inputs = InputParamList(p1, p2, p3, p4, p5),
+                          inputs = InputParamList(p1, p2, p3, p4, p5, p6),
                           outputs = OutputParamList(o1, o2, o3, o4, o5, o6))
