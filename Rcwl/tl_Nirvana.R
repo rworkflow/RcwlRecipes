865846d641f9780a139546a96eb951318d00f63b
Qian Liu <qliu7@buffalo.edu>
2023-01-05 22:44:23 -0500

    Update tl_Nirvana.R

--- Rcwl/tl_Nirvana.R
+++ Rcwl/tl_Nirvana.R
@@ -1,13 +1,15 @@
 ## https://illumina.github.io/NirvanaDocumentation/
-p1 <- InputParam(id = "cache", type = "Directory", prefix = "-c")
+p1 <- InputParam(id = "cache", type = "Directory", position = -1)
 p2 <- InputParam(id = "sd", type = "Directory", prefix = "--sd")
 p3 <- InputParam(id = "ref", type = "File", prefix = "-r")
 p4 <- InputParam(id = "prefix", type = "string", prefix = "-o")
-o1 <- OutputParam(id = "out", type = "File", glob = "$(inputs.prefix).gz",
+p5 <- InputParam(id = "vcf", type = "File", prefix = "-i")
+o1 <- OutputParam(id = "out", type = "File", glob = "$(inputs.prefix).json.gz",
                   secondaryFile = ".jsi")
 req1 <- requireDocker("annotation/nirvana:3.14")
 Nirvana <- cwlProcess(cwlVersion = "v1.2",
                       baseCommand = c("dotnet", "/opt/nirvana/Nirvana.dll"),
+                      arguments = list("-c", list(valueFrom="$(inputs.cache.path)/Both")),
                       requirements = list(req1),
-                      inputs = InputParamList(p1, p2, p3, p4),
+                      inputs = InputParamList(p1, p2, p3, p4, p5),
                       outputs = OutputParamList(o1))
