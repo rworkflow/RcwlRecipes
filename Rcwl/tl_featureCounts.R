2a4893e56952920e5e2d1ec51ee83df7b3122db5
qhu <qiang.hu@roswellpark.org>
2024-04-20 15:36:33 -0400

    update featureCounts version

--- Rcwl/tl_featureCounts.R
+++ Rcwl/tl_featureCounts.R
@@ -5,7 +5,7 @@ f2 <- InputParam(id = "count", type = "string", prefix = "-o")
 f3 <- InputParam(id = "bam", type = "File")
 o1 <- OutputParam(id = "Count", type = "File", glob = "$(inputs.count)", secondaryFiles = ".summary")
 
-req1 <- requireDocker("quay.io/biocontainers/subread:2.0.1--h7132678_2")
+req1 <- requireDocker("quay.io/biocontainers/subread:2.0.6--he4a0461_0")
 featureCounts <- cwlProcess(baseCommand = "featureCounts",
                             arguments = list("-p", "--countReadPairs"),
                             requirements = list(req1),
